e98a6976d4fc3a02e0960786e97ecb8c754cf1ed
Sumit Jadhav <sumitjune123@gmail.com>
2025-06-01 15:42:31 +0530

    Delete bro code directory

--- bro code/DOM/script.js
+++
@@ -1,9 +0,0 @@
-// DOM = Document Object Model 
-// object{} that represents the page you see in the web browser and provides you with An API to interact with it 
-// web browser constructs the DOM when it loads an html document,
-// and structures all the elements in a tree-like representation. JavaScript can also access the DOM dynamically change the content , structure , and style of a web page .
-
-const username = "Sumit ";
-const welcomeMsg = document.getElementById("welcome-msg");
-welcomeMsg.textContent += username === "" ? `Guest` : username;
-console.log(document);
--- bro code/For loops/script.js
+++
@@ -1,7 +0,0 @@
-for(let i = 1; i <= 20 ; i ++){
-    if(i == 13){
-        break;
-    }else{
-        console.log(i);
-    }
-}
